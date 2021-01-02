ca111ac990db8faa8355b844351992f36e95f870
Nick Yahnke <nick@pitchinteractive.com>
2021-01-02 15:10:36 -0800

    better message chunking

--- chunkString.js
+++ chunkString.js
@@ -1,3 +1,7 @@
-module.exports = function chunkString(str, length) {
-  return str.match(new RegExp('.{1,' + length + '}', 'g'));
+module.exports = function chunkString(str, chunkSize) {
+  const chunks = []
+  for (let pos = 0; pos < str.length; pos += chunkSize) {
+    chunks.push(str.slice(pos, pos + chunkSize))
+  }
+  return chunks
 }
